9914d3e3168ef1c66890c59b8caf9e0f298a7ef4
Tayyab Haseeb <73613576+tayyabhaseeb@users.noreply.github.com>
2023-11-08 23:53:50 +0500

    Add files via upload

---
+++ Map.jsx
@@ -0,0 +1,51 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useContextValue } from "../Contexts/CitiesContext";
+
+function Map() {
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { citiesData } = useContextValue();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(() => {
+    if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+  }, [mapLat, mapLng]);
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {citiesData.map((obj) => (
+          <Marker position={[obj.position.lat, obj.position.lng]} key={obj.id}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+      </MapContainer>
+    </div>
+  );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+export default Map;
